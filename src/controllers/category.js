e61524c03386ebb561355638188962ce2aa5c509
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-12-27 18:12:42 +0700

    test

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,6 +1,6 @@
 const { json } = require('body-parser')
 const model = require('../models/category')
-const { Success, NOT, Failed } = require('../helpers/response')
+const { Success, Failed } = require('../helpers/response')
 const controller = {
     getAllCategory: (req, res) => {
         model.getAllCategory()
