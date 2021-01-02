1a5803ed0b55c5a5e86041523fd5f2f36bc3e8e9
bagaskaramadhan <bagaskaramadhan@gmail.com>
2021-01-02 16:59:16 +0700

    change router patch

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,7 @@ route
     .get('/books/getAll', getAll)
     .get('/books/getByid/:id', getByid)
     .post('/books/insert', insert)
-    .put('/books/update/:id', update)
+    .patch('/books/update/:id', update)
     .delete('/books/delete/:id', delet)
     // CATEGORY
     .get('/category/getAll', getAllCategory)
